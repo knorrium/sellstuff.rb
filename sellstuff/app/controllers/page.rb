a9369a522fd5f3a2dc3f6e21acc36d405b3f8ec2
Felipe Knorr Kuhn <git@knorrium.info>
2013-02-14 22:01:13 -0200

    added the pages controller

---
+++ sellstuff/app/controllers/page.rb
@@ -0,0 +1,15 @@
+Sellstuff.controllers :page do
+
+  get :about, :map => '/about' do
+    render :erb, 'page/about'
+  end
+
+  get :contact , :map => "/contact" do
+    render :erb, 'page/contact'
+  end
+
+  get :home, :map => "/" do
+    render :erb, 'page/home'
+  end
+
+end
