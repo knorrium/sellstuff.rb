dc2f63fc592fde4db9eef2aa50063e60da54ac3f
Felipe Knorr Kuhn <git@knorrium.info>
2013-03-16 12:19:34 -0300

    fixed item status migration

--- sellstuff/db/migrate/011_add_item_id_to_item_status.rb
+++ sellstuff/db/migrate/011_add_item_id_to_item_status.rb
@@ -1,12 +1,12 @@
 class AddItemIdToItemStatus < ActiveRecord::Migration
   def self.up
-    change_table :itemstatuses do |t|
+    change_table :item_statuses do |t|
       t.integer :item_id
     end
   end
 
   def self.down
-    change_table :itemstatuses do |t|
+    change_table :item_statuses do |t|
       t.remove :item_id
     end
   end
